b88d1cb7dc97b277db85b16263a8bdea8c1ec67a
cwb528 <43507996+cwb528@users.noreply.github.com>
2025-08-20 17:29:41 +0800

    Update comm_score.R

--- R/comm_score.R
+++ R/comm_score.R
@@ -234,7 +234,7 @@ lr_score <- function( exp,meta.data,sample,celltype,
   meta.data <- data.frame( meta.data )
   samples <-  meta.data[[sample]] %>% unique() %>% as.character()
   celltypes <-  meta.data[[celltype]] %>% unique() %>% as.character()
-  message(  paste0( "Samples: ",paste( sort(celltypes) ,collapse = ', ' )   )  )
+  message(  paste0( "Samples: ",paste( sort(samples) ,collapse = ', ' )   )  )
   message(  paste0( "Cell types: ",paste( sort(celltypes) ,collapse = ', ' )   )  )
 
   message( format(Sys.time(), "%Y-%m-%d %H:%M:%S") , ' | ','Checking the expression profiles of ligands and receptors.'  )
